dc1c370c656deb090922cb29beeb7730a3d4349e
maleroy2604 <martleroy@msn.com>
2024-02-02 12:50:14 +0100

    add reading file and check file

--- tasktracker.py
+++ tasktracker.py
@@ -1,23 +1,37 @@
+import os.path
 from datetime import datetime
 
 
 class TaskTracker:
 
+    TODAY_DATE = datetime.now().strftime("%Y-%m-%d")
+
     def __init__(self):
-        self.tasks = []
+        self._tasks = []
+        self._file_name = f"tasks_{self.TODAY_DATE}.txt"
 
     def start_daily_tasks_process(self):
+        if os.path.exists(self._file_name):
+            content = self.read_from_daily_file()
+            print(f"Current tasks: \n {content}")
         while True:
             input_user = input("Enter your daily task (type exit to stop prompt): ")
             if input_user == "exit":
                 break
-            self.tasks.append(input_user)
+            self._tasks.append(input_user)
 
         self.write_into_daily_file()
 
     def write_into_daily_file(self):
-        today_date = datetime.now().strftime("%Y-%m-%d")
-        file_name = f"tasks_{today_date}.txt"
-        with open(file_name, 'w') as file:
-            for task in self.tasks:
-                file.write(task + '\n')
+        if os.path.exists(self._file_name):
+            with open(self._file_name, 'a') as file:
+                for task in self._tasks:
+                    file.write(task + '\n')
+        else:
+            with open(self._file_name, 'w') as file:
+                for task in self._tasks:
+                    file.write(task + '\n')
+
+    def read_from_daily_file(self):
+        with open(self._file_name, 'r') as file:
+            return file.read()
